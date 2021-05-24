f1bf3fa50cd3abd0ae6d00e50962ed43e7a7417c
majharul-909 <mmfreefire123@gmail.com>
2021-05-24 17:55:34 +0600

    new
    sad

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,6 +174,23 @@
     });
     });
 
+// Testimonials carousel (uses the Owl Carousel library)
+$(".testimonials-carousel").owlCarousel({
+  autoplay: true,
+  dots: true,
+  loop: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    768: {
+      items: 2
+    },
+    900: {
+      items: 3
+    }
+  }
+});
 
  
 
